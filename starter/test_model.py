de47f07f38d81c528128d49a748e80978b6072fb
thanhta <thanhta2010@gmail.com>
2024-02-17 19:04:58 -0500

    Add logging to test_model.py

--- starter/test_model.py
+++ starter/test_model.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pandas as pd
+import logging
 import pytest
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from starter.ml.data import process_data
 from starter.ml.model import train_model, inference, compute_model_metrics
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
 
 cat_features = [
     "workclass",
@@ -20,6 +25,7 @@ cat_features = [
 
 def test_model():
     """ Test Random Forest model """
+    logger.info('Invoking test_model')
 
     X = np.random.rand(100, 5)
     y = np.random.randint(2, size=100)
@@ -31,6 +37,8 @@ def test_inference():
     """
     Test inference of model
     """
+    logger.info('Invoking test_inference')
+
     X = np.random.rand(100, 5)
     y = np.random.randint(2, size=100)
     model = train_model(X, y)
@@ -41,6 +49,8 @@ def test_compute_model_metrics():
     """
     Test compute_model_metrics using precision, recall, and F1
     """
+    logger.info('test_compute_model_metrics')
+    
     y = [1, 1, 1, 0]
     y_preds = [1, 0, 1, 0]
     out_put = compute_model_metrics(y, y_preds)
