aa4572fba5bef2ba62e019968ff16dfb1ea784d7
thanhta <thanhta2010@gmail.com>
2024-02-17 23:23:41 -0500

    Restructure the direcootry by elimiating the top directory: start to match with Github action directory

--- main.py
+++ main.py
@@ -47,24 +47,24 @@ model_path_pkl = 'model/model.pkl'
 #model_path = os.path.join(file_dir, model_path_pkl)
 model_path = os.path.join(root_dir, model_path_pkl)
 logger.info(f"model_path: {model_path}")
-#model = joblib.load(model_path)#
+model = joblib.load(model_path)#
             
 logger.info('Retrieve Encoder')
 encoder_path_pkl = 'model/encoder.pkl'
 #encoder_path = 'model/encoder.pkl'
 #encoder_path = os.path.join(file_dir, encoder_path_pkl)
 encoder_path = os.path.join(root_dir, encoder_path_pkl)
-#encoder = joblib.load(encoder_path)   
+encoder = joblib.load(encoder_path)   
 
 logger.info('Retrieve LabelBinarizer')
 lb_path_pkl = 'model/lb.pkl'
 #lb_path = 'model/lb.pkl'
 #lb_path = os.path.join(file_dir, lb_path)
 lb_path = os.path.join(root_dir, lb_path_pkl)
-#lb = joblib.load(lb_path)
+lb = joblib.load(lb_path)
 
 
-model, encoder, lb = get_pkl()
+#model, encoder, lb = get_pkl()
 
 # Declare the data object
 class InputData(BaseModel):
